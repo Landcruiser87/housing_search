963db72d0a997343346c6441333d4039f7609a93
Landcruiser87 <andyheroy@gmail.com>
2021-12-31 12:10:54 -0600

    updates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   <b> Chicago Housing Search Tool </b><br>
 </h1>
 
-This repo is meant to coalesce different housing resources to aide in searching for housing in Chicago.  The goal is to collect all the listings daily, then upload them to a gsheet that i can sift through individually.  We'll also be making use of [rapidapi](https://rapidapi.com) to make calls to the zillow api, then run manual searches on the other two sites.   
+This repo is meant to coalesce different housing resources to aide in searching for housing in Chicago.  The goal is to collect all the listings daily, then upload them to a gsheet that i can sift through individually.  
 
 ### Requirements
 - Python >= 3.8
@@ -18,5 +18,10 @@ This repo is meant to coalesce different housing resources to aide in searching 
 
 ### Sites to search
 - [] - [Zillow](https://www.zillow.com)
-- [] - [Apartments.com](https://www.apartments.com)
+- [] - [craigslist.com](https://www.craiglist.org)
 - [] - [rent.com](https://www.rent.com)
+
+### External Data Sources
+- [CTA L stops](https://data.cityofchicago.org/Transportation/CTA-System-Information-List-of-L-Stops/8pix-ypme/data)
+- [CTA Crime data](https://data.cityofchicago.org/Public-Safety/Gun-Crimes-Heat-Map/iinq-m3rg)
+- 
--- scripts/zillow.py
+++ scripts/zillow.py
@@ -2,16 +2,15 @@
 
 import requests
 import numpy as np
+import pandas as pd
 import json
 from datetime import date, datetime
 import time
 import sys
-import re
 import os
 
+#%%
 
-
-#Before we start requesting , lets setup what neighborhoods we want to look in. 
 cities = [
 	'Ravenswood',
 	'Lincoln Square',
@@ -21,20 +20,73 @@ cities = [
 	'Winnemac'
 ]
 
-#Set the header for pulling
-header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36" }
+state = 'IL'
+zip = '60640'
 
-#Results list
-results = []
+#Before we start requesting , lets setup what neighborhoods we want to look in. 
+url = 'https://www.zillow.com/homes/for_rent/Ravenswood,-Chicago,-IL_rb'
 
-for city in cities:
-	time.sleep(3)
-	base_url = f'https://www.zillow.com/homes/{city},-Chicago,-IL_rb/'
-	response = requests.get(base_url, headers = header )
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+  'referer': 'https://www.zillow.com/homes/for_rent/Ravenswood,-Chicago,-IL_rb/?searchQueryState=%7B%22pagination'
+  
+}
 
-	resp_json = response.json()
-	results.append(resp_json)
-	# print(resp_json)
-	# print(resp_json['searchResults']['listResults'][0]['hdpUrl'])
+response_html = requests.get(url, headers=header)
+
+from lxml import html
+
+resp_html = html.fromstring(response_html.text)
+html.open_in_browser(doc=resp_html, encoding='UTF-8')
+
+
+
+#Notes on request.
+# html format
+# encoding - UTF-8
+
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #Results list
+# results = []
+
+# base_url = f'https://www.zillow.com/homes/Ravenswood,-Chicago,-IL_rb/'
+# response = requests.get(base_url, headers = header )
+# print(response.status_code)
+
+# for city in cities:
+# 	time.sleep(3)
+# 	base_url = f'https://www.zillow.com/homes/{city},-Chicago,-IL_rb/'
+# 	response = requests.get(base_url, headers = header )
+
+# 	resp_json = response.json()
+# 	results.append(resp_json)
+# 	# print(resp_json)
+# 	# print(resp_json['searchResults']['listResults'][0]['hdpUrl'])
 
 #%%
