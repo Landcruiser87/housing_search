631b07d4c9aeb526252e5a7d8f93bc37f99eebf5
Landcruiser87 <andyheroy@gmail.com>
2022-01-02 07:26:13 -0600

    Switching to using soup.  womp womp

--- scripts/craigslist.py
+++ scripts/craigslist.py
@@ -1,9 +1,12 @@
+#%%
 # Resources
 # https://betterprogramming.pub/how-i-built-a-python-scraper-to-analyze-housing-locations-on-craigslist-part-1-18d264b0ec4b
 # https://www.dataquest.io/blog/apartment-finding-slackbot/
+# https://github.com/lewi0622/Housing_Search
 
 
 import requests
+from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 import json
@@ -11,47 +14,68 @@ from datetime import date, datetime
 import time
 import sys
 import os
-from craigslist import CraigslistHousing
 
-#Data pull from craigslist
-response = CraigslistHousing(
-	site='chicago', 
-	area='chc', 
-	filters={
-		'max_price': 2300, 
-		'min_price': 500,
-		'min_bedrooms': 2,
-		'has_image': True,
-		'dogs_ok': True,
-		'laundry': {'w/d in unit','laundry in bldg', 'laundry on site'},
-	}
+
+cookies = {
+    'cl_b': '4|01390574a6c854a2c021189123aa48e017cc4d1d|1641081102E_6YY',
+    'cl_tocmode': 'hhh%3Agrid',
+}
+
+headers = {
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+params = (
+    ('hasPic', '1'),
+    ('min_price', '500'),
+    ('max_price', '2400'),
+    ('min_bedrooms', '2'),
+    ('availabilityMode', '0'),
+    ('pets_dog', '1'),
+    ('laundry', ['1', '4', '2', '3']),
+    ('sale_date', 'all dates'),
 )
 
+response = requests.get('https://chicago.craigslist.org/search/chc/apa', headers=headers, params=params, cookies=cookies)
 
-#results are going to be huge, so probably need to chunk out the results. 
+#NB. Original query string below. It seems impossible to parse and
+#reproduce query strings 100% accurately so the one below is given
+#in case the reproduced version is not "correct".
+# response = requests.get('https://chicago.craigslist.org/search/chc/apa?hasPic=1&min_price=500&max_price=2400&min_bedrooms=2&availabilityMode=0&pets_dog=1&laundry=1&laundry=4&laundry=2&laundry=3&sale_date=all+dates', headers=headers, cookies=cookies)
 
-#Max record count. 
-MAX_RECORDS = int(response.get_results_approx_count())
+bs4ob = BeautifulSoup(response.text, 'lxml')
+links = []
 
-#Don't want to piss off craigslist.  so put a limit. 
-if MAX_RECORDS > 3000:
-	MAX_RECORDS = 100
+for link in soup.find_all('li', {'class': 'result-row'}):
+	links.append(link.get('href'))
 
-listings = response.get_results(sort_by='newest', geotagged=True, limit = 20)
+
+
+
+
 
 with open('../data/total_search_area.txt', 'r') as search_coords:
 	data = search_coords.read().splitlines()[0]
 	lat1, lng1, lat2, lng2 = eval(data)
 
 
-# for listing in listings:
-# 	pass
 
 
 
-#Pull out all the results at once.  Sort by newest. 
-#remove listings that aren't in the total search bound box. 
-#
+#%%
 
 
 
@@ -82,40 +106,32 @@ with open('../data/total_search_area.txt', 'r') as search_coords:
 
 
 
-# cookies = {
-#     'cl_b': '4|335be9d38652077f88e297d559c4e3ceddea7e6e|1640949278y5-qM',
-#     'cl_def_hp': 'chicago',
-#     'cl_tocmode': 'hhh%3Agrid',
-# }
 
-# headers = {
-#     'Connection': 'keep-alive',
-#     'Pragma': 'no-cache',
-#     'Cache-Control': 'no-cache',
-#     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-#     'sec-ch-ua-mobile': '?0',
-#     'sec-ch-ua-platform': '"Windows"',
-#     'Upgrade-Insecure-Requests': '1',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'Sec-Fetch-Site': 'same-site',
-#     'Sec-Fetch-Mode': 'navigate',
-#     'Sec-Fetch-User': '?1',
-#     'Sec-Fetch-Dest': 'document',
-#     'Referer': 'https://nmi.craigslist.org/',
-#     'Accept-Language': 'en-US,en;q=0.9',
-# }
 
-# params = (
-#     ('availabilityMode', '0'),
-#     ('pets_dog', '1'),
-#     ('postal', '60640'),
-#     ('search_distance', '3'),
+
+
+# #Data pull from craigslist
+# response = CraigslistHousing(
+# 	site='chicago', 
+# 	area='chc', 
+# 	filters={
+# 		'max_price': 2300, 
+# 		'min_price': 500,
+# 		'min_bedrooms': 2,
+# 		'has_image': True,
+# 		'dogs_ok': True,
+# 		'laundry': {'w/d in unit','laundry in bldg', 'laundry on site'},
+# 	}
 # )
 
-# response = requests.get('https://chicago.craigslist.org/d/apartments-housing-for-rent/search/apa', headers=headers, params=params, cookies=cookies)
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-# response = requests.get('https://chicago.craigslist.org/d/apartments-housing-for-rent/search/apa?availabilityMode=0&pets_dog=1&postal=60640&search_distance=3', headers=headers, cookies=cookies)
+# #results are going to be huge, so probably need to chunk out the results. 
+
+# #Max record count. 
+# MAX_RECORDS = int(response.get_results_approx_count())
+
+# #Don't want to piss off craigslist.  so put a limit. 
+# if MAX_RECORDS > 3000:
+# 	MAX_RECORDS = 100
+
+# listings = response.get_results(sort_by='newest', geotagged=True, limit = 20)
