61b267d1ebc48b112873869a3d15f80ec74b223b
Landcruiser87 <andyheroy@gmail.com>
2022-01-08 17:49:30 -0600

    name change support.py to mail_send.py

--- scripts/mail_send.py
+++ scripts/mail_send.py
@@ -2,7 +2,7 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-url = 'https://chicago.craigslist.org/chc/apa/d/chicago-spacious-1770-square-foot/7430117801.html'
+url = 'https://chicago.craigslist.org/chc/apa/d/chicago-super-affordable-lincoln-square/7420817455.html'
 
 
 def docstring_parameter(*sub):
