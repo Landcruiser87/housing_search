e34ad194815ba7846da9767bc82c008ce63495b4
Landcruiser87 <andyheroy@gmail.com>
2025-04-07 13:00:39 -0500

    reset runtime config

--- scripts/renthousing/main.py
+++ scripts/renthousing/main.py
@@ -28,8 +28,8 @@ from support import logger, console, log_time
 # pound sign to the right of neighborhood means its a city of chicago neighborhood, 
 # if doesn't have one, its a smaller targeted neighborhood.
 AREAS = [
-    'Irving Park',     #
     'Ravenswood',
+    'Irving Park',     #
     'Portage Park',    #
     'Albany Park',     #
     'North Center',    #
@@ -192,7 +192,7 @@ def scrape(neigh:str, progbar, task, layout):
     Args:
         neigh (str): Neighborhood or Zipcode
     # """	
-    # shuffle(SITES) #Keep em guessin!
+    shuffle(SITES) #Keep em guessin!
     for source in SITES:
         site = SOURCES.get(source)
         if site:
@@ -294,7 +294,7 @@ def main():
         logger.warning("No historical data found")
 
     #Shuffle and search the neighborhoods/zips
-    # shuffle(AREAS)
+    shuffle(AREAS)
 
     with Live(layout, refresh_per_second=30, screen=True, transient=True):
         logger.addHandler(support.MainTableHandler(main_table, layout, logger.level))
