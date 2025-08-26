0fcec3d1f6cc60bbaf81012b4a5adea07c68ef9b
landcruiser87 <andyheroy@gmail.com>
2025-08-26 08:39:10 -0500

    update buy_list json

--- scripts/buyhousing/map_results.py
+++ scripts/buyhousing/map_results.py
@@ -7,12 +7,19 @@
 # might have to use a ref list and keep track of the idx as a global var
 # 
 # Optional sort conditions.  
-# 1. Be able to sort by most recent, or oldest
-# 2. Have a pop up that shows listing information...
-# 3. Color code cities/zips by amount of listings found.  
-# 4. Have a time window select of the last day, week, month.
-# 5. Be able to save a home so that it always shows up
-    # when you're searching.  
-# 6. Export button for saved homes.  
-# 7. Filter by site.
-# 8. If duplicate addresses found... just show it on zillow?
+    # 1. Be able to sort by most recent, or oldest
+    # 2. Have a pop up that shows listing information...
+    # 3. Color code cities/zips by amount of listings found.  
+    # 4. Have a time window select of the last day, week, month.
+    # 5. Be able to save a home so that it always shows up
+        # when you're searching.  
+    # 6. Export button for saved homes.  
+    # 7. Filter by site.
+    # 8. If duplicate addresses found, maybe color code the point to how many of the 4 sites its on.   
+    # 9. Or have the ability to select quantiles of attributes by site.  
+        # - Available
+        # - Sqft
+        # - price_sqft
+        # - price change
+        # - price
+        # - lotsqft 
