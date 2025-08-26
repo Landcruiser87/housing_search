0566d0be8d20321709da3b36bab9a9f9aa50b166
landcruiser87 <andyheroy@gmail.com>
2025-08-25 19:17:27 -0500

    [feature] - mapping
    Adding a mapping feature for results.

--- scripts/buyhousing/map_results.py
+++ scripts/buyhousing/map_results.py
@@ -1,3 +1,18 @@
 #TODO -
 # Build this file out to visualize and sort through different dates
 # I'd like to have a date select function.  Make it like the slider.py
+
+# Same basic grid layout as slider.py, but the bottom timeline now
+# is controlled by datetime.  Not sure how the units will work for that
+# might have to use a ref list and keep track of the idx as a global var
+# 
+# Optional sort conditions.  
+# 1. Be able to sort by most recent, or oldest
+# 2. Have a pop up that shows listing information...
+# 3. Color code cities/zips by amount of listings found.  
+# 4. Have a time window select of the last day, week, month.
+# 5. Be able to save a home so that it always shows up
+    # when you're searching.  
+# 6. Export button for saved homes.  
+# 7. Filter by site.
+# 8. If duplicate addresses found... just show it on zillow?
